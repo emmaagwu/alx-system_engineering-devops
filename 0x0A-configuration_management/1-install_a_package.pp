e64a0f34fbe50a40bf61543445aa15c2a9d1acea
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-03-24 01:19:24 +0100

    manifest to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-# Install puppet-lint
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Installs a specific version of flask(2.1.0)
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
